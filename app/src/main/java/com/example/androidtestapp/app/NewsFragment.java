744e138e7fc6c175c36d3fd0bfe59b2777a17cfe
Nicola Tarocco <Nicola.Tarocco@nespresso.com>
2014-10-23 17:45:34 +0200

    Added Gradle flavours

--- app/src/main/java/com/example/androidtestapp/app/NewsFragment.java
+++ app/src/main/java/com/example/androidtestapp/app/NewsFragment.java
@@ -4,8 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
@@ -13,20 +13,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.util.ArrayList;
-
-public class NewsFragment extends ListFragment {
+public class NewsFragment extends Fragment {
 
     private static final String LOG_TAG = "NEWS_FRAGMENT";
     private ArrayAdapter<String> newsListAdapter;
 
 
     private FragmentDisplayer mFragmentDisplayer;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
 //
 //    @Override
 //    public void onCreate(Bundle savedInstanceState) {
@@ -53,13 +58,13 @@ public class NewsFragment extends ListFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        newsListAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, new String[] {});
-        setListAdapter(newsListAdapter);
-
-        Log.i(LOG_TAG, "Registering receiver");
-        IntentFilter statusIntentFilter = new IntentFilter(Constants.BROADCAST_ACTION);
-        mFragmentDisplayer = new FragmentDisplayer();
-        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mFragmentDisplayer, statusIntentFilter);
+//        newsListAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, new String[] {});
+//        setListAdapter(newsListAdapter);
+//
+//        Log.i(LOG_TAG, "Registering receiver");
+//        IntentFilter statusIntentFilter = new IntentFilter(Constants.BROADCAST_ACTION);
+//        mFragmentDisplayer = new FragmentDisplayer();
+//        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mFragmentDisplayer, statusIntentFilter);
     }
 
 
@@ -74,25 +79,25 @@ public class NewsFragment extends ListFragment {
             if (intent.hasExtra(Constants.STATUS)) {
                 Log.i(LOG_TAG, "***************** UPDATED STATUS in fragment:" + intent.getStringExtra(Constants.STATUS));
 
-                String newsString = intent.getStringExtra(Constants.STATUS);
-                JSONArray jsonNewsList = new JSONArray();
-                try {
-                    jsonNewsList = new JSONArray(newsString);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                ArrayList<String> newsTitlesList = new ArrayList<>();
-                for (JSONObject jsonNews : jsonNewsList) {
-                    try {
-                        newsTitlesList.add(jsonNews.getString("title"));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                newsListAdapter.clear();
-                newsListAdapter.addAll();
+//                String newsString = intent.getStringExtra(Constants.STATUS);
+//                JSONArray jsonNewsList = new JSONArray();
+//                try {
+//                    jsonNewsList = new JSONArray(newsString);
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                ArrayList<String> newsTitlesList = new ArrayList<>();
+//                for (JSONObject jsonNews : jsonNewsList) {
+//                    try {
+//                        newsTitlesList.add(jsonNews.getString("title"));
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//
+//                newsListAdapter.clear();
+//                newsListAdapter.addAll();
 
             }
         }
